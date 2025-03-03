46e467dae4a8d04df54fb2a060e144d682ef406d
Mike Kihiu <mikekihiu@icloud.com>
2025-03-03 08:52:21 +0300

    Initial Commit

---
+++ CoinRanking/CoinRankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankingApp.swift
+//  CoinRanking
+//
+//  Created by Mike Kihiu on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinRankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinRankingTests/CoinRankingTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankingTests.swift
+//  CoinRankingTests
+//
+//  Created by Mike Kihiu on 03/03/2025.
+//
+
+import Testing
+@testable import CoinRanking
+
+struct CoinRankingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
